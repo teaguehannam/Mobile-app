45411d2d331cd170b6722b373a71cb9c2360ba2c
Teague Hannam <te@gue.email>
2021-06-28 10:40:45 -0600

    Fix universe photo library

--- src/data/LoadImages.js
+++ src/data/LoadImages.js
@@ -7,6 +7,6 @@ AllPhotos['Universe'] = [];
 for(var i = 1; i < 14; i++) { AllPhotos['AnimalPhotos'].push({ id:i, src:'Animal (' + i + ').jpg' }) };
 for(var i = 1; i < 10; i++) { AllPhotos['InfoPhotos'].push({ id:i, src:'Info(' + i + ').jpg' }) };
 for(var i = 1; i < 73; i++) { AllPhotos['PlacesPhotos'].push({ id:i, src:'Place (' + i + ').jpg' }) };
-for(var i = 1; i < 51; i++) { AllPhotos['Universe'].push({ id:i, src:'Universe (' + i + ').jpg' }) };
+for(var i = 1; i < 49; i++) { AllPhotos['Universe'].push({ id:i, src:'Universe (' + i + ').jpg' }) };
 
 export default AllPhotos;
